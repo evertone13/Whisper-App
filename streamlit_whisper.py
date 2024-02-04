5c1750e2977631c5a8e58ca7a05622f97bc83a38
evertone13 <102230880+evertone13@users.noreply.github.com>
2024-02-04 15:05:09 +0800

    Update streamlit_whisper.py

--- streamlit_whisper.py
+++ streamlit_whisper.py
@@ -6,17 +6,19 @@ st.title("Whisper App")
 # upload audio file with streamlit
 audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
 
-model=whisper.load_model("base")
+model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
-    
+
+
 if st.sidebar.button("Transcribe Audio"):
     if audio_file is not None:
         st.sidebar.success("Transcribing Audio")
-        transcription =model.transcribe(audio_file.name)
+        transcription = model.transcribe(audio_file.name)
         st.sidebar.success("Transcription Complete")
-        st.text(transcription["text"])
+        st.markdown(transcription["text"])
     else:
-        str.sidebar.error("Please upload an audio file")
+        st.sidebar.error("Please upload an audio file")
+
 
 st.sidebar.header("Play Original Audio File")
 st.sidebar.audio(audio_file)
